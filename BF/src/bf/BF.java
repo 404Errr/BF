99c8d5e4d08df1a2c87dd48a594ed9d61558e9de
404Errr <auggieam@gmail.com>
2017-03-27 20:08:03 -0400

    Update BF.java

--- BF/src/bf/BF.java
+++ BF/src/bf/BF.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BF {
-	private static final String RAW = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+//	private static final String RAW = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 //	private static final String RAW = "";
-	
-	private static final boolean PRINT_DATA = false;
+	private static final String RAW = "+>+[<[>>+>+<<<-]>>>[<<<+>>>-]<<[>+>+<<-]>>[<<+>>-]<]";//"+>+ [< [>>+>+<<<-]>>>[<<<+>>>-]copy <<[>+>+<<-]>>[<<+>>-]add <]"
+
+	private static final boolean PRINT_DATA = true;
 	private static final boolean OUTPUT_NUMBER = false;
 	
 	private static final char LEFT = '<', RIGHT = '>';
@@ -17,13 +18,15 @@ public class BF {
 	private static final char[] LEGAL = {LEFT, RIGHT, PLUS, MINUS, OUTPUT, INPUT, LOOP_OPEN, LOOP_CLOSE};
 
 	private static char[] code;
-	private static byte[] data;
+//	private static byte[] data;
+	private static int[] data;
 	private static int codePointer, pointer;
 
 	public static void main(String[] args) {
 		compile(RAW);
 //		System.out.println(Arrays.toString(code));
-		data = new byte[30000];
+		data = new int[30000];
+//		data = new byte[30000];
 		for (int i = 0;i<data.length;i++) {
 			data[i] = 0;
 		}
@@ -32,20 +35,10 @@ public class BF {
 
 	private static void checkValidBrackets(List<Character> brackets) {
 		int pairCount = 0;
-//		System.out.println(pairCount);
 		for (int i = 0;i<brackets.size();i++) {
 			if (brackets.get(i)==LOOP_OPEN) pairCount++;//is [
 			else pairCount--;//is ]
-//			System.out.println(pairCount+"\t"+(char) brackets.get(i));
 		}
-		if (pairCount!=0) {
-			System.err.println("bracket error");
-			System.exit(0);
-		}
-//		if (brackets.size()%2!=0) {
-//			System.err.println("bracket error");
-//			System.exit(0);
-//		}
 		try {
 			while (brackets.size()>=2) {
 				int j = 0;
@@ -55,7 +48,7 @@ public class BF {
 			}
 		}
 		catch (Exception e) {}
-		if (brackets.size()>0) {
+		if (brackets.size()>0||pairCount!=0) {
 			System.err.println("bracket error");
 			System.exit(0);
 		}
@@ -88,96 +81,53 @@ public class BF {
 //			System.out.println((char) code[codePointer]+"\t"+codePointer);
 			switch (code[codePointer]) {
 			case LEFT:
-				left();
+				if (pointer>0) pointer--;
 				break;
 			case RIGHT:
-				right();
+				if (pointer<data.length-1) pointer++;
 				break;
 			case PLUS:
-				plus();
+				data[pointer]++;
 				break;
 			case MINUS:
-				minus();
+				data[pointer]--;
 				break;
 			case OUTPUT:
-				output();
+				if (OUTPUT_NUMBER) System.out.println(data[pointer]);
+				else System.out.print((char) data[pointer]);
 				break;
 			case INPUT:
-				input();
+				//todo
 				break;
 			case LOOP_OPEN:
-				loopOpen();
+				if (data[pointer]==0) {
+					int depth = 0;
+					do {
+						if (code[codePointer]!=LOOP_CLOSE) depth++;
+						if (code[codePointer]!=LOOP_OPEN) depth--;
+						codePointer++;
+					} while (depth!=0);
+				}
 				break;
 			case LOOP_CLOSE:
-				loopClose();
+				if (data[pointer]!=0) {
+					int depth = 0;
+					do {
+						if (code[codePointer]!=LOOP_CLOSE) depth++;
+						if (code[codePointer]!=LOOP_OPEN) depth--;
+						codePointer--;
+					} while (depth!=0);
+				}
 				break;
 			}
 			if (PRINT_DATA) printData();
 			codePointer++;
 		}
-	}
+	}	
 
-	private static void loopOpen() {
-//		if (data[pointer]==0) {
-//			while (code[codePointer]!=LOOP_CLOSE) {
-//				codePointer++;
-//			}
-//		}
-		if (data[pointer]==0) {
-			int depth = 0;
-			do {
-				if (code[codePointer]!=LOOP_CLOSE) depth++;
-				if (code[codePointer]!=LOOP_OPEN) depth--;
-				codePointer++;
-			} while (depth!=0);
-		}
-	}
-	
-	private static void loopClose() {
-//		if (data[pointer]!=0) {
-//			while (code[codePointer]!=LOOP_OPEN) {
-//				codePointer--;
-//			}
-//		}
-		if (data[pointer]!=0) {
-			int depth = 0;
-			do {
-				if (code[codePointer]!=LOOP_CLOSE) depth++;
-				if (code[codePointer]!=LOOP_OPEN) depth--;
-				codePointer--;
-			} while (depth!=0);
-		}
-	}
-	
-	private static void right() {
-		if (pointer<data.length-1) pointer++;
-	}
-
-	private static void left() {
-		if (pointer>0) pointer--;
-	}
-
-	private static void plus() {
-		data[pointer]++;
-	}
-
-	private static void minus() {
-		data[pointer]--;
-	}
-
-	private static void output() {
-		if (OUTPUT_NUMBER) System.out.println(data[pointer]);
-		else System.out.print((char) data[pointer]);
-	}
-
-	private static void input() {//TODO
-
-	}
-	
 	private static final int PRINT_LENGTH = 30;
 	private static void printData() {
 		StringBuilder str = new StringBuilder();
-		
 		for (int i = 0;i<PRINT_LENGTH;i++) {
 			if (i==pointer) {
 				str.append("[");
@@ -190,16 +140,6 @@ public class BF {
 			if (data[i]<=15) str.append("0");
 			str.append(Integer.toHexString(data[i]));
 		}
-		System.out.println((char) code[codePointer]+"\t"+str.toString());
+		System.out.println(str.toString());
 	}
 }
-/*
-> right
-< left
-+ add 1
-- subtract 1
-. output
-, input
-[ loop start (end when current is 0)
-] loop end
-*/
